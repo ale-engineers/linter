4c6b5396351605d456a047e33c5c1717a2d06fc9
junki-pw <juken.master1234@gmail.com>
2025-02-17 13:47:13 +0900

    commit

---
+++ lib/generator/item.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'item.freezed.dart';
+part 'item.g.dart';
+
+@freezed
+abstract class Item with _$Item {
+  const factory Item({
+    String? id,
+  }) = _Item;
+
+  const Item._();
+
+  factory Item.fromJson(Map<String, dynamic> json) => _$ItemFromJson(json);
+}
